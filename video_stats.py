ac3560023ee0711afd670b918b607d77da12ba3f
francesco-dota <francesco.dota@gmail.com>
2026-02-10 10:48:35 +0100

    added new function get_playlust_id

--- video_stats.py
+++ video_stats.py
@@ -10,6 +10,7 @@ load_dotenv(dotenv_path="C:/Courses/YT_ELT/venv/.env")
 API_KEY = os.getenv("API_KEY")
 #print(API_KEY)
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 
 def get_playlist_id():
@@ -32,8 +33,44 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+
+
+def get_video_ids(playlist_id):
+    video_ids = []
+    pageToken = None
+    
+    #base_url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&id={playlist_id}&maxResults={maxResults}&key={API_KEY}"
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlist_id}&key={API_KEY}"
+
+    print("get_video_ids is running")
+    
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url+=f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+                
+            pageToken = data.get("nextPageToken")
+            
+            if not pageToken:
+                break        
+
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+
 if __name__ == "__main__":
     print("get_playlist_id will be executed")
-    get_playlist_id()        
+    playlist_id = get_playlist_id()     
+    print("playlist_id will be executed")
+    get_video_ids(playlist_id)
 else:    
     print("get_playlist_id won't be executed")          
